dca30d22d0db42d7dafca3a09f13a02147666c21
prath <prathamgi.pg@gmail.com>
2021-10-04 21:13:31 +0530

    added trips and testimonials

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -1,10 +1,13 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components"
 
-export const GlobalStyle = createGlobalStyle `
-* {
-    font-family: 'Roboto', sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Ephesis&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&display=swap');
+
+    * {
+        font-family: 'Roboto', sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from "../components/Hero"
+import Trips from "../components/Trips"
+import Testimonials from "../components/Testimonials"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
-     </Layout>
+    <Trips heading="Our Favourite Destinations" />
+    <Testimonials />
+  </Layout>
 )
 
 export default IndexPage
