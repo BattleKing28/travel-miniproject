5333a1dac050da651d78ce65c094c4329c731868
prath <prathamgi.pg@gmail.com>
2021-10-13 13:24:03 +0530

    merged changes from master

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import "./styles/about.css"
 
 const about = () => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Seo from "../components/seo"
 import Hero from "../components/Hero"
 import Trips from "../components/Trips"
 import Testimonials from "../components/Testimonials"
+import Stats from "../components/Stats"
+import Email from "../components/Email"
+import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
@@ -12,6 +15,9 @@ const IndexPage = () => (
     <Hero />
     <Trips heading="Our Favourite Destinations" />
     <Testimonials />
+    <Stats />
+    <Email />
+    <Footer />
   </Layout>
 )
 
